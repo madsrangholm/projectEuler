1f7499af9e4fe20c03d6c3704c719d4889492719
madsrangholm <madsrangholm@gmail.com>
2015-08-29 19:37:48 +0200

    Problem001 solved

---
+++ Problem001/Problem001.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem001
+{
+    public class Problem001
+    {
+        public static int Execute()
+        {
+            var sum = 0;
+            for (var i = 0; i < 1000; i++)
+            {
+                if (i%3 == 0 || i%5 == 0)
+                {
+                    sum += i;
+                }
+            }
+            return sum;
+        }
+    }
+}
---
+++ Problem001Tests/Problem001Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Problem001Tests
+{
+    [TestClass]
+    public class Problem001Tests
+    {
+        [TestMethod]
+        public void Test()
+        {
+            Assert.AreEqual(233168, Problem001.Problem001.Execute());
+        }
+    }
+}
