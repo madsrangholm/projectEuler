a35ef60e810df98c0c8817d397b4584d94886ab6
madsrangholm <madsrangholm@gmail.com>
2015-09-05 19:12:01 +0200

    Problem007 added

---
+++ Problem007/Code.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Problem007
+{
+    public static class Code
+    {
+        public static long FindPrime(long index)
+        {
+            var count = 1;
+            var number = 2;
+            while (count < index)
+            {
+                number++;
+                if (IsPrime(number))
+                {
+                    count++;
+                }
+                if (count == index) break;
+            }
+            return number;
+        }
+
+        private static bool IsPrime(long num)
+        {
+            var numSquared = Convert.ToInt64(Math.Sqrt(num));
+            for (var i = 2; i <= numSquared; i++)
+            {
+                if (num%i == 0) return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ Tests/0xx/007/Problem007Tests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Problem007;
+
+namespace Tests._0xx._007
+{
+    [TestClass]
+    public class Problem007Tests
+    {
+        [TestMethod, TestCategory("Assumption")]
+        public void _007FindPrime6()
+        {
+            var expectedResult = 13;
+            var result = Code.FindPrime(6);
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod, TestCategory("Solution")]
+        public void _007Solution()
+        {
+            var expectedResult = 104743;
+            var result = Code.FindPrime(10001);
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
