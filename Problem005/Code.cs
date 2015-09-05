d590546932bded6ed0e5f40b7edfba3fd88f6349
madsrangholm <madsrangholm@gmail.com>
2015-09-05 17:41:22 +0200

    Problem005 added

---
+++ Problem005/Code.cs
@@ -0,0 +1,25 @@
+﻿namespace Problem005
+{
+    public static class Code
+    {
+        public static long SmallestMultiple(int min, int max)
+        {
+            var testedNumber = max;
+            while (true)
+            {
+                for (var i = min; i <= max; i++)
+                {
+                    if (testedNumber%i != 0)
+                    {
+                        break;
+                    }
+                    if (i == max)
+                    {
+                        return testedNumber;
+                    }
+                }
+                testedNumber++;
+            }
+        }
+    }
+}
---
+++ Tests/0xx/005/Problem005Tests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Problem005;
+
+namespace Tests._0xx._005
+{
+    [TestClass]
+    public class Problem005Tests
+    {
+        [TestMethod, TestCategory("Assumption")]
+        public void _005TestSmallestMultiple1_10()
+        {
+            var expectedResult = 2520;
+            var result = Code.SmallestMultiple(1, 10);
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod, TestCategory("Solution")]
+        public void _005Solution()
+        {
+            var expectedResult = 232792560;
+            var result = Code.SmallestMultiple(1, 20);
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
