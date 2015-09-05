c546ab099121ccd82ad976a3cc134b0078b584ac
madsrangholm <madsrangholm@gmail.com>
2015-09-05 22:31:46 +0200

    Problem008 added

---
+++ Problem008/Code.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace Problem008
+{
+    public class Code
+    {
+        public static long LargestProduct(string series, int numDigits)
+        {
+            long largestProduct = 0;
+            for (var i = 0; i < series.Count() - numDigits; i++)
+            {
+                var numArray = series.Substring(i, numDigits).Select(x => Convert.ToInt64(x.ToString()));
+                var product = numArray.Aggregate((long) 1, (current, num) => current*num);
+                if (largestProduct < product)
+                {
+                    largestProduct = product;
+                }
+            }
+            return largestProduct;
+        }
+    }
+}
