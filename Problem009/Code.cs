6e7e557bf98483b4f6321189e5b022deffbf7bf9
madsrangholm <madsrangholm@gmail.com>
2015-09-06 11:58:37 +0200

    Problem009 added

---
+++ Problem009/Code.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Problem009
+{
+    public static class Code
+    {
+        public static long PytagorianTripletProduct(int sum)
+        {
+            for (long a = 1; a < sum; a++)
+            {
+                for (var b = a + 1; b < sum; b++)
+                {
+                    var c = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+                    if (c == Math.Floor(c) && a + b + Convert.ToInt64(c) == sum)
+                    {
+                        return a*b*Convert.ToInt64(c);
+                    }
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ Tests/0xx/009/Problem009Tests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Problem009;
+
+namespace Tests._0xx._009
+{
+    [TestClass]
+    public class Problem009Tests
+    {
+        [TestMethod, TestCategory("Assumption")]
+        public void _008PytagorianTriplet3_4_5()
+        {
+            var expectedResult = 60;
+            var result = Code.PytagorianTripletProduct(12);
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod, TestCategory("Solution")]
+        public void _009Solution()
+        {
+            var expectedResult = 31875000;
+            var result = Code.PytagorianTripletProduct(1000);
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
