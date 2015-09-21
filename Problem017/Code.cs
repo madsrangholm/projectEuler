5a9271257241b3561e3d0495bf058e3a2eb7dd0a
madsrangholm <madsrangholm@gmail.com>
2015-09-21 10:32:51 +0200

    Problem017 added

---
+++ Problem017/Code.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem017
+{
+    public class Code
+    {
+        private static readonly IDictionary<int, string> texts = new Dictionary<int, string>()
+        {
+            { 0, "" },
+            { 1, "one" },
+            { 2, "two" },
+            { 3, "three" },
+            { 4, "four" },
+            { 5, "five" },
+            { 6, "six" },
+            { 7, "seven" },
+            { 8, "eight" },
+            { 9, "nine" },
+            { 10, "ten" },
+            { 11, "eleven" },
+            { 12, "twelve" },
+            { 13, "thirteen" },
+            { 14, "fourteen" },
+            { 15, "fifteen" },
+            { 16, "sixteen" },
+            { 17, "seventeen" },
+            { 18, "eighteen" },
+            { 19, "nineteen" },
+            { 20, "twenty" },
+            { 30, "thirty" },
+            { 40, "forty" },
+            { 50, "fifty" },
+            { 60, "sixty" },
+            { 70, "seventy" },
+            { 80, "eighty" },
+            { 90, "ninety" },
+            { 100, "hundred" },
+            { 1000, "thousand" }
+        };
+
+        public static int LetterCountSum(int min, int max)
+        {
+            var sum = 0;
+            for(var i= min; i <= max; i++)
+            {
+                sum += LetterCount(i);
+            }
+            return sum;
+        }
+        public static int LetterCount(int num)
+        {
+            var text = NumToText(num);
+            Console.WriteLine(text);
+            return text.Replace(" ", "").Count();
+        }
+        private static string NumToText(int num)
+        {
+            if(num >= 1000)
+            {
+                return texts[num / 1000] + texts[1000] + NumToText(num % 1000);
+            }
+            if(num >= 100)
+            {
+                return texts[num / 100] + " " + texts[100] + (num%100 != 0 ? " and " : "") + NumToText(num % 100);
+            }
+            if(num >= 20)
+            {
+                return texts[(num / 10) * 10] + " " + NumToText(num % 10);
+            }
+            return texts[num];
+        }
+    }
+}
---
+++ Tests/0xx/017/Problem017Tests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Problem017;
+
+namespace Tests._0xx._017
+{
+    [TestClass]
+    public class Problem017Tests
+    {
+        [TestMethod, TestCategory("Assumption")]
+        public void _017LetterCountSum1_5()
+        {
+            var expectedResult = 19;
+            var result = Code.LetterCountSum(1, 5);
+            Assert.AreEqual(expectedResult, result);
+        }
+        [TestMethod, TestCategory("Assumption")]
+        public void _017LetterCount342()
+        {
+            var expectedResult = 23;
+            var result = Code.LetterCount(342);
+            Assert.AreEqual(expectedResult, result);
+        }
+        [TestMethod, TestCategory("Assumption")]
+        public void _017LetterCount115()
+        {
+            var expectedResult = 20;
+            var result = Code.LetterCount(115);
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod, TestCategory("Solution")]
+        public void _017Solution()
+        {
+            var expectedResult = 21124;
+            var result = Code.LetterCountSum(1,1000);
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
