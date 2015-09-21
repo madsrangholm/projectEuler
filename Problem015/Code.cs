864b157ef7ebe9185b8606d3f62feeb4723f7d9d
madsrangholm <madsrangholm@gmail.com>
2015-09-21 13:44:36 +0200

    Problem015 added.

---
+++ Problem015/Code.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Problem015
+{
+    public static class Code
+    {
+        public static long LatticeRoutes(int xDim, int yDim)
+        {
+            var array = new long[xDim + 1, yDim + 1];
+            array[0, 0] = 1;
+            for (var i = 1; i <= xDim + yDim; i++)
+            {
+                for (var x = 0; x <= xDim; x++)
+                {
+                    for (var y = 0; y <= yDim; y++) //This loop should be eliminated.
+                    {
+                        if (x + y == i)
+                        {
+                            array[x, y] = (x > 0 ? array[x - 1, y] : 0) + (y > 0 ? array[x, y - 1] : 0);
+                            break;
+                        }
+                    }
+                }
+            }
+            return array[xDim, yDim];
+        }
+    }
+}
---
+++ Tests/0xx/015/Problem015Tests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Problem015;
+
+namespace Tests._0xx._015
+{
+    [TestClass]
+    public class Problem015Tests
+    {
+        [TestMethod, TestCategory("Assumption")]
+        public void _015LatticePaths2_2()
+        {
+            var expectedResult = 6;
+            var result = Code.LatticeRoutes(2,2);
+            Assert.AreEqual(expectedResult, result);
+        }
+        [TestMethod, TestCategory("Solution")]
+        public void _015Solution()
+        {
+            var expectedResult = 137846528820;
+            var result = Code.LatticeRoutes(20, 20);
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
