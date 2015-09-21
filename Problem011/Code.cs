43b0e9d2cea4afcd9a7d944f45628d4940e7e1bd
madsrangholm <madsrangholm@gmail.com>
2015-09-21 16:56:25 +0200

    Problem011 work-in-progress

---
+++ Problem011/Code.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem011
+{
+    public static class Code
+    {
+        public static long LargestProductInGrid(int[,] grid, int rowLength)
+        {
+            long largestProduct = 0;
+            for (var x = 0; x < grid.GetLength(0); x++)
+            {
+                for (var y = 0; y < grid.GetLength(1); y++)
+                {
+                    var pointMax = PointMaximum(grid, rowLength, x, y);
+                    if (largestProduct < pointMax)
+                    {
+                        largestProduct = pointMax;
+                    }
+                }
+            }
+        }
+
+        private static long PointMaximum(int[,] grid, int rowLength, int x, int y)
+        {
+            //check right
+            if (x + rowLength < grid.GetLength(0))
+            {
+
+                //check right up
+                if (0 <= y - rowLength)
+                {
+                    
+                }
+                if (y + rowLength < grid.GetLength(1))
+                {
+                    
+                }
+            }
+            
+
+            //check right down
+            //check down
+        }
+    }
+}
